403c9043bac1cc9b00ee6406e19afa7cb1c5e6b5
Renato D. Schneider <rdaltibaschneider@gmail.com>
2022-05-03 12:37:05 -0400

    style(styled-components): configure styles basic in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Header } from './components/Header'
+import Login from './components/Login'
+
 function App() {
-  return <h1>dasd</h1>
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
